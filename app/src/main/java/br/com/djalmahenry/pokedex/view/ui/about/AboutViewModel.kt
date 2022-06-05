ee04bec92eb1d527a3c1fd312e7ec752575ab426
Djalma Henrique <djalma@geppetto.com.br>
2022-06-05 17:46:07 -0300

    feat: add about page

--- app/src/main/java/br/com/djalmahenry/pokedex/view/ui/about/AboutViewModel.kt
+++ app/src/main/java/br/com/djalmahenry/pokedex/view/ui/about/AboutViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class AboutViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "Colaboradores"
     }
     val text: LiveData<String> = _text
 }

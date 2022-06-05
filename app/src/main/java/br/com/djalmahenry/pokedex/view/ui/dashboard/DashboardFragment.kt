c9e45a463081a5500ec3390caa7562ec6812e7de
Djalma Henrique <djalma@geppetto.com.br>
2022-06-05 00:44:44 -0300

    feat: add view model

--- app/src/main/java/br/com/djalmahenry/pokedex/view/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/br/com/djalmahenry/pokedex/view/ui/dashboard/DashboardFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.observe
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import br.com.djalmahenry.pokedex.R
@@ -14,63 +16,37 @@ import br.com.djalmahenry.pokedex.api.PokemonRepository
 import br.com.djalmahenry.pokedex.domain.Pokemon
 import br.com.djalmahenry.pokedex.domain.PokemonType
 import br.com.djalmahenry.pokedex.view.PokemonAdapter
+import br.com.djalmahenry.pokedex.viewmodel.PokemonViewModel
+import br.com.djalmahenry.pokedex.viewmodel.PokemonViewModelFactory
 
 class DashboardFragment : Fragment() {
 
     private lateinit var dashboardViewModel: DashboardViewModel
     private lateinit var recyclerView: RecyclerView
 
+    private val viewModel by lazy {
+        ViewModelProvider(this, PokemonViewModelFactory())
+            .get(PokemonViewModel::class.java)
+    }
+
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        dashboardViewModel =
-                ViewModelProvider(this).get(DashboardViewModel::class.java)
+        dashboardViewModel = ViewModelProvider(this).get(DashboardViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
         recyclerView = root.findViewById(R.id.rvPokemons)
 
-//        val charmander = Pokemon(
-//            "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/004.png",
-//            1,
-//            "Charmander",
-//            listOf(PokemonType("Fire"))
-//        )
-//
-//        val pokemons = listOf(charmander, charmander, charmander, charmander)
-
-        Thread(Runnable {
-            loadPokemons()
-        }).start()
+        viewModel.pokemons.observe(viewLifecycleOwner, Observer {
+            loadRecyclerView(it)
+        })
 
         return root
     }
 
-    private fun loadPokemons() {
-        val pokemonsApiResult = PokemonRepository.listPokemons()
-        pokemonsApiResult?.results?.let {
-            val pokemons: List<Pokemon?> = it.map { pokemonResult ->
-                val number = pokemonResult.url
-                    .replace("https://pokeapi.co/api/v2/pokemon/", "")
-                    .replace("/", "").toInt()
-                val pokemonApiResult = PokemonRepository.getPokemon(number)
-
-                pokemonApiResult?.let {
-                    Pokemon(
-                        pokemonApiResult.id,
-                        pokemonApiResult.name,
-                        pokemonApiResult.types.map { type ->
-                            type.type
-                        }
-                    )
-                }
-            }
-            val layoutManager = LinearLayoutManager(getContext())
-
-            recyclerView.post {
-                recyclerView.layoutManager = layoutManager
-                recyclerView.adapter = PokemonAdapter(pokemons)
-            }
-        }
+    private fun loadRecyclerView(pokemons: List<Pokemon?>) {
+        recyclerView.layoutManager = LinearLayoutManager(getContext())
+        recyclerView.adapter = PokemonAdapter(pokemons)
     }
 }
